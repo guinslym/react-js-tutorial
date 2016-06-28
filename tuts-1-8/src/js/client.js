09fddc21d7ea34fe8f0cf7419c5106915f44cd31
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-28 01:24:09 -0400

    adding Footer and Header.js

--- tuts-1-8/src/js/client.js
+++ tuts-1-8/src/js/client.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//import Layout from "./components/Layout";
-class Layout extends  React.Component{
-  constructor(){
-    super();
-    this.name = "Guinsly!!!"
-}
+import Layout from "./components/Layout";
 
-  render(){
-    return (
-      <h1>It's {this.name}</h1>
-    );
-  }
-}
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
