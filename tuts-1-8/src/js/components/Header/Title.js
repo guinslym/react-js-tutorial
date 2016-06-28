ae814f30f48efcbe4a2f34f86b2158c43b7ef630
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-28 01:38:55 -0400

    adding props to pass variable to components

--- tuts-1-8/src/js/components/Header/Title.js
+++ tuts-1-8/src/js/components/Header/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Title extends  React.Component{
   render(){
     return (
-      <h1>Welcome G!</h1>
+      <h1>{this.props.title}</h1>
     );
   }
 }
--- tuts-1-8/src/js/components/Layout.js
+++ tuts-1-8/src/js/components/Layout.js
@@ -4,9 +4,11 @@ import Header from "./Header";
 
 export default class Layout extends  React.Component{
   render(){
+  const title = 'Guinsly Mondésir';
     return (
       <div>
-        <Header />
+        <Header title={title} />
+        <Header title={"Other Title"} />
         <Footer />
       </div>
     );
