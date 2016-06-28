3a79b2dc763c17b8b724a820c67b719dd7bd9e4f
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-28 01:26:53 -0400

    Video #3 Done!

--- tuts-1-8/src/js/components/Header.js
+++ tuts-1-8/src/js/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Title from "./Header/Title";
 
 export default class Header extends React.Component{
   render(){
     return (
-      <header>Hello</header>
+      <Title/>
     );
   }
 }
