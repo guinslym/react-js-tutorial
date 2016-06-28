bb20b608431fb8e57caf68df8676b1296d7f01b9
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-28 01:42:16 -0400

    Video #4 Done

--- tuts-1-8/src/js/components/Layout.js
+++ tuts-1-8/src/js/components/Layout.js
@@ -3,7 +3,16 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends  React.Component{
+  constructor(){
+    super();
+    this.state =  {
+    title: 'Welcome...',
+    }
+  }
   render(){
+  setTimeout(()=>{
+    this.setState({title: "Welcome Kizyah!!!"}, 2000);
+});
   const title = 'Guinsly Mondésir';
     return (
       <div>
